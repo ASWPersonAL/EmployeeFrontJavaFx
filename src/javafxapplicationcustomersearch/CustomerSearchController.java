c6ed6f0bf981d06d473fae399f91034715e00f31
ASW <ASW@192.168.0.14>
2018-12-02 15:54:27 +0100

    The employyList is shown in JavaFx UI with GET method for REST webservice.

--- src/javafxapplicationcustomersearch/CustomerSearchController.java
+++ src/javafxapplicationcustomersearch/CustomerSearchController.java
@@ -6,11 +6,20 @@
 package javafxapplicationcustomersearch;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 
 /**
  *
@@ -22,14 +31,47 @@ public class CustomerSearchController implements Initializable {
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TextField textFieldSearch;
+    
+    @FXML
+    private Button buttonSearch;
+    
+    @FXML
+    private TableView<Customer> tableView;
+    
+    //@FXML
+    //private void handleButtonAction(ActionEvent event) {
+      //  System.out.println("You clicked me!");
+        //label.setText("Hello World!");
+    //}
+    
+    @FXML
+   private void handleSearchAction(){
+       WebTarget clientTarget;
+       ObservableList<Customer> data = tableView.getItems();
+       data.clear();
+       Client client = ClientBuilder.newClient();
+       client.register(CustomerMessageBodyReader.class);
+       if(textFieldSearch.getText().length() > 0){
+           clientTarget = client.target("http://localhost:8080/ServerSide/webresources/customer/search/{beginBy}");
+           clientTarget = clientTarget.resolveTemplate("beginBy", textFieldSearch.getText());
+       }else{
+           clientTarget = client.target("http://localhost:8080/ServerSide/webresources/customer");
+       }
+       GenericType<List<Customer>> listc = new GenericType<List<Customer>>() {
+            };
+       List<Customer> customers = clientTarget.request("application/json").get(listc);
+       
+       for(Customer c : customers){
+           data.add(c);
+           System.out.println(c.toString());
+       }
+       
+   }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        handleSearchAction();
     }    
     
 }
